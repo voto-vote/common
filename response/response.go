b781a9360bc8b80110494c0b20394ba986deda04
Simon Strobel <simon@voto.vote>
2022-02-18 10:31:01 +0100

    [VOTO] Added global repsonses

--- response/response.go
+++ response/response.go
@@ -1,6 +1,24 @@
 package response
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/voto-vote/common"
+)
+
+func HandleError(status int32, errorMsg string) (events.APIGatewayProxyResponse, error) {
+	errResponse := common.ErrorResponse{
+		Status:  status,
+		Message: errorMsg,
+	}
+	jsoned, _ := json.Marshal(errResponse)
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       string(jsoned),
+	}, nil
+}
 
 func HandleSuccess(code int, body string) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
