cde2298952d5647f289f4c296d2bc38381d32e1d
Simon Strobel <simon@voto.vote>
2022-02-18 11:27:29 +0100

    [VOTO] Added firebase

--- auth/firebase.go
+++ auth/firebase.go
@@ -2,13 +2,11 @@ package auth
 
 import (
 	"context"
-	"log"
 	"os"
 	"strings"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
 )
 
@@ -22,21 +20,15 @@ func SetupFirebase() Firebase {
 	new := strings.Replace(env, "`", "", -1)
 
 	opt := option.WithCredentialsJSON([]byte(new))
-	logrus.Info("Created options")
 	ctx := context.Background()
-	logrus.Info("Created context")
 	//Firebase admin SDK initialization
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
-		logrus.Error("Firebase load error: " + err.Error())
 		panic("Firebase load error: " + err.Error())
 	}
 	//Firebase Auth
-	logrus.Info("Created new app")
 	auth, err := app.Auth(ctx)
-	logrus.Info("Created new app auth")
 	if err != nil {
-		logrus.Error("Firebase app load error: " + err.Error())
 		panic("Firebase load error: " + err.Error())
 	}
 	f := Firebase{
@@ -48,7 +40,6 @@ func SetupFirebase() Firebase {
 }
 
 func (f *Firebase) CreateAuthUser(email string, phone string, pwd string, name string) (*auth.UserRecord, error) {
-	log.Println("Create Auth User in firebase")
 	params := (&auth.UserToCreate{}).
 		Email(email).
 		EmailVerified(false).
@@ -56,14 +47,10 @@ func (f *Firebase) CreateAuthUser(email string, phone string, pwd string, name s
 		DisplayName(name).
 		Disabled(false)
 	u, err := f.Auth.CreateUser(context.Background(), params)
-	if err != nil {
-		log.Println("Could not create auth user: ", err.Error())
-	}
 	return u, err
 }
 
 func (f *Firebase) DeleteAuthUser(email string) error {
-	log.Println("Delete Auth User in firebase")
 	authuser, err := f.Auth.GetUserByEmail(context.Background(), email)
 	if err != nil {
 		return err
