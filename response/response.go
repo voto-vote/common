ce0a24b8b0be359054db063b1ee98662decb06ac
strobsi <simon@voto.vote>
2022-04-05 11:00:27 +0200

    [VOTO] Added cors allow for each success

--- response/response.go
+++ response/response.go
@@ -27,8 +27,18 @@ func HandleError(status int32, errorMsg string) (events.APIGatewayProxyResponse,
 }
 
 func HandleSuccess(code int, body string) (events.APIGatewayProxyResponse, error) {
+
+	headers := map[string]string{
+		"Access-Control-Allow-Headers":     "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+		"Access-Control-Allow-Origin":      "*",
+		"Access-Control-Allow-Methods":     "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+		"Access-Control-Allow-Credentials": "true",
+		"X-Requested-With":                 "*",
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: code,
 		Body:       body,
+		Headers:    headers,
 	}, nil
 }
