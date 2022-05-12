bb933eedc95aca5a8f953e5d7a55f9bf4fd97b72
strobsi <simon@voto.vote>
2022-05-12 08:43:50 +0200

    [VOTO] Add cors headers also to error responses

--- response/response.go
+++ response/response.go
@@ -8,6 +8,14 @@ import (
 	"github.com/voto-vote/common"
 )
 
+var CORS_HEADERS = map[string]string{
+	"Access-Control-Allow-Headers":     "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+	"Access-Control-Allow-Origin":      "*",
+	"Access-Control-Allow-Methods":     "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+	"Access-Control-Allow-Credentials": "true",
+	"X-Requested-With":                 "*",
+}
+
 func HandleError(status int32, errorMsg string) (events.APIGatewayProxyResponse, error) {
 	errResponse := common.ErrorResponse{
 		Status:  status,
@@ -16,11 +24,13 @@ func HandleError(status int32, errorMsg string) (events.APIGatewayProxyResponse,
 	jsoned, err := json.Marshal(errResponse)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
+			Headers:    CORS_HEADERS,
 			StatusCode: http.StatusInternalServerError,
 			Body:       string(jsoned),
 		}, err
 	}
 	return events.APIGatewayProxyResponse{
+		Headers:    CORS_HEADERS,
 		StatusCode: http.StatusInternalServerError,
 		Body:       string(jsoned),
 	}, nil
@@ -28,17 +38,9 @@ func HandleError(status int32, errorMsg string) (events.APIGatewayProxyResponse,
 
 func HandleSuccess(code int, body string) (events.APIGatewayProxyResponse, error) {
 
-	headers := map[string]string{
-		"Access-Control-Allow-Headers":     "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-		"Access-Control-Allow-Origin":      "*",
-		"Access-Control-Allow-Methods":     "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
-		"Access-Control-Allow-Credentials": "true",
-		"X-Requested-With":                 "*",
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: code,
 		Body:       body,
-		Headers:    headers,
+		Headers:    CORS_HEADERS,
 	}, nil
 }
